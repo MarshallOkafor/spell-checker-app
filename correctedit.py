685b071f18df9f464d827ee5cd56145808626c0e
GOKUL NAGA PRASAD SUDANAGUNTA <89621701+SUDANGOKUL@users.noreply.github.com>
2021-10-13 04:20:27 -0500

    Update correctedit.py
    the Comments are added to the Program to explain and ease the understanding of the program.

--- correctedit.py
+++ correctedit.py
@@ -1,16 +1,16 @@
-from gingerit.gingerit import GingerIt
+from gingerit.gingerit import GingerIt #Gingerit is used to ease the file collection of dictionaries and to compare the data to the database.
 print("Enter the Text")
-data=open("C:\python\demo.txt").read()
+data=open("C:\python\demo.txt").read() #sources the input text from input file and can be changed to automated input string by Flask
 list=[]
-result = GingerIt().parse(data)
-corrections = result['corrections']
-correctText = result['result']
+result = GingerIt().parse(data) #method to compare the text to the database and lets the miss spelt words.
+corrections = result['corrections'] #return the misspelt words
+correctText = result['result'] #return the  corrected misspelt words
 
-print("Correct Text:",correctText)
+print("Correct Text:",correctText) 
 print()
-print("CORRECTIONS")
+print("CORRECTIONS")                           
 for d in corrections:
-  if d['text'] == d['correct']:
+  if d['text'] == d['correct']: 
         print()
   else:
         print("________________")    
@@ -18,4 +18,4 @@ for d in corrections:
         print("Correction:",d['correct'])
         list.append(d['correct'])        
         
-print(list, file=open("output.txt", "a"))
+print(list, file=open("output.txt", "a")) #returns the output to the external file
